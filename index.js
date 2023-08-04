20a8a2b678e07203fbe1088852a74ea6bae4e1f1
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-08-05 00:11:40 +0530

    master

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
-
 server.use(jsonServer.bodyParser);
+
 server.use((req, res, next) => {
   if (req.method === "GET" && req.query._sort) {
     const order = req.query._order || "asc";
@@ -15,7 +15,6 @@ server.use((req, res, next) => {
   }
   next();
 });
-
 server.use((req, res, next) => {
   if (req.method === "GET" && req.query.category) {
     const category = req.query.category;
@@ -28,5 +27,33 @@ server.use((req, res, next) => {
   next();
 });
 
+server.post("/products", (req, res) => {
+  const newProduct = req.body;
+  newProduct.id =
+    router.db
+      .get("products")
+      .reduce((maxId, product) => Math.max(maxId, product.id), 0) + 1; // Generate a new ID
+  router.db.get("products").push(newProduct).write();
+  res.status(201).json(newProduct);
+});
+
+server.delete("/products/:id", (req, res) => {
+  const productId = parseInt(req.params.id);
+  router.db.get("products").remove({ id: productId }).write();
+  res.sendStatus(204);
+});
+
+server.put("/products/:id", (req, res) => {
+  const productId = parseInt(req.params.id);
+  const updatedProduct = req.body;
+  router.db
+    .get("products")
+    .find({ id: productId })
+    .assign(updatedProduct)
+    .write();
+  res.status(200).json(updatedProduct);
+});
+
 server.use(router);
+
 server.listen(port);
